7ff0296762b2c33830c01b6f0613f1c4923955cf
TZ-Porky <btkextensions@gmail.com>
2025-06-07 23:55:15 +0100

    Add initial screen and widget files for Home, Service, Settings, and OptionCard

---
+++ lib/option_card_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OptionCardWidget extends StatefulWidget {
+  const OptionCardWidget({super.key});
+
+  @override
+  State<OptionCardWidget> createState() => _OptionCardWidgetState();
+}
+
+class _OptionCardWidgetState extends State<OptionCardWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
